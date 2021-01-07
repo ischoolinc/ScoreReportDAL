60f078db27e5792eeba0ea672f2c1563a908a6ad
chuntaichen <ct.chen@ischool.com.tw>
2021-01-07 18:24:42 +0800

    開發中，先遷入一版備份。

---
+++ SHCourseGroupCodeDAL/StudentCourseCodeInfo.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SHCourseGroupCodeDAL
+{
+    public class StudentCourseCodeInfo
+    {
+        public string SchoolYear { get; set; }
+        public string Semester { get; set; }
+
+        public string StudentID { get; set; }
+
+        public string CourseGroupCode { get; set; }
+
+        List<SubjectInfo> SubjectInfoList = new List<SubjectInfo>();
+
+        public void AddSubjectInfo(SubjectInfo subj)
+        {
+            SubjectInfoList.Add(subj);
+        }
+
+        public Dictionary<string, SubjectInfo> GetSubjectInfoDict()
+        {
+            Dictionary<string, SubjectInfo> value = new Dictionary<string, SubjectInfo>();
+
+            foreach (SubjectInfo si in SubjectInfoList)
+            {
+                if (!value.ContainsKey(si.GetSubjectKey()))
+                    value.Add(si.GetSubjectKey(), si);
+            }
+
+            return value;
+        }
+
+        public void ParseCourseCode(Dictionary<string, string> SourceDict)
+        {
+            foreach (SubjectInfo si in SubjectInfoList)
+            {
+                if (SourceDict.ContainsKey(si.GetSubjectKey()))
+                {
+                    // 比對資料後填入
+                    si.SetCode(CourseGroupCode, SourceDict[si.GetSubjectKey()]);
+                }
+
+            }
+        }
+    }
+}
+
---
+++ SHCourseGroupCodeDAL/SubjectInfo.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SHCourseGroupCodeDAL
+{
+    public class SubjectInfo
+    {
+        public SubjectInfo(string subjectName, string entryYear, string requireBy, string required)
+        {
+            SubjectName = subjectName;
+            EntryYear = entryYear;
+            RequireBy = requireBy;
+            Required = required;
+
+            // 科目名稱+校部定+必選修
+            SubjectKey = SubjectName + "_" + RequireBy + "_" + Required;
+        }
+
+
+        /// <summary>
+        /// 比對用
+        /// </summary>
+        private string SubjectKey = "";
+        /// <summary>
+        /// 科目名稱
+        /// </summary>
+        private string SubjectName = "";
+
+        /// <summary>
+        /// 入學年
+        /// </summary>
+        private string EntryYear = "";
+
+        /// <summary>
+        /// 校部定
+        /// </summary>
+        private string RequireBy = "";
+
+        /// <summary>
+        /// 必選修
+        /// </summary>
+        private string Required = "";
+
+        /// <summary>
+        /// 群組代碼
+        /// </summary>
+        private string GroupCode = "";
+
+
+        /// <summary>
+        /// 課程代碼
+        /// </summary>
+        private string CourseCode = "";
+
+        public string GetSubjectKey()
+        {
+            return SubjectKey;
+        }
+
+        public string GetCourseCode()
+        {
+            return CourseCode;
+        }
+
+
+        public string GetSubjectName()
+        {
+            return SubjectName;
+        }
+
+        public string GetRequireBy() { return RequireBy; }
+
+        public string GetRequired() { return Required; }
+
+
+        public void SetCode(string groupCode, string courseCode)
+        {
+            GroupCode = groupCode;
+            CourseCode = courseCode;
+        }
+    }
+}
